52fb760823d9a804e90daad7f7d402a4ef7d86b1
kennmark <skyhex27@gmail.com>
2023-10-23 04:58:07 -0400

    change port

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
-export const PORT = 5555 // localdev
-// export const PORT = 'https://skyph-hackerlists.onrender.com/' //production
+export const PORT = 443 // localdev
 
 export const mongoDBURL =
   'mongodb+srv://root:ksabalberino%4027@skyphhackerlist.mldux1n.mongodb.net/hackergroup-lists?retryWrites=true&w=majority'
