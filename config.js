e705f1d7d7e5d04f08337b90ac627a70ed217f7f
kennmark <skyhex27@gmail.com>
2023-10-23 13:55:36 -0400

    change port number

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-export const PORT = 443 // localdev
+export const PORT = 5555 // localdev
 
 export const mongoDBURL =
   'mongodb+srv://root:ksabalberino%4027@skyphhackerlist.mldux1n.mongodb.net/hackergroup-lists?retryWrites=true&w=majority'
